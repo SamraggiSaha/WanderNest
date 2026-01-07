e2a5c8e5d9a1007e85314bd6e3364b8a96f24681
SamraggiSaha <sahasamraggi2005@gmail.com>
2026-01-07 12:44:51 +0530

    AirBnB Clone

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get("/",(req,res)=>{
     //});
     // await sampleListing.save();
     // console.log("Sample listing saved");
-     //res.send("successful testing");
+      // res.send("successful testing");
  //});
 app.use("/listings",listings);
 app.use("/listings/:id/reviews",reviews);
