eec7fd882c56ad208ce3db9ff48d8b798b21aed6
SamraggiSaha <sahasamraggi2005@gmail.com>
2026-01-16 22:10:04 +0530

    AirBnB Clone

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.get("/",(req,res)=>{
 });
  // app.get("/testlisting",async(req,res)=>{
     // let sampleListing = new Listing({
-          // title:"My Villa",
+            //title:"My Villa",
                  //description:"A beautiful villa by the sea",
         //price:1200,
         //location:"Calangute,Goa",
