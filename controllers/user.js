2f044e620b4fd0f3898cb1f51727f33d5784c06d
SamraggiSaha <sahasamraggi2005@gmail.com>
2026-04-12 01:10:19 +0530

    AirBnB Clone

---
+++ controllers/user.js
@@ -0,0 +1,42 @@
+const Listing = require('../models/listing.js');
+const User = require('../models/user.js');
+
+module.exports.RenderSignUpForm = (req, res) => {
+    res.render("users/signUp.ejs");
+};
+module.exports.signUp = async(req,res)=>{
+    try{
+   let {username,email,password} = req.body;
+   const newuser = new User({email,username});
+   const registeredUser = await User.register(newuser,password);
+   console.log(registeredUser);
+   req.login(registeredUser,(err)=>{
+    if(err){
+        return next(err);
+    }
+    req.flash("success","Welcome to WanderNest!");
+    res.redirect("/listings");
+   });
+    }
+    catch(e){
+        req.flash("error",e.message);
+        res.redirect("/signUp");
+    }
+   
+};
+module.exports.RenderLoginForm = (req,res)=>{
+    res.render("users/login.ejs");
+};
+module.exports.login = async(req,res)=>{
+    req.flash("success","Welcome back!");
+    res.redirect(res.locals.redirectUrl || "/listings");
+};
+module.exports.logout = (req,res,next)=>{
+    req.logout((err)=>{
+        if(err){
+            next(err);
+        }
+        req.flash("success","You are logged out now!");
+        res.redirect("/listings");
+    });
+};
--- routes/user.js
+++ routes/user.js
@@ -4,43 +4,16 @@ const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
-router.get("/signUp", (req, res) => {
-    res.render("users/signUp.ejs");
-});
-router.post("/signUp", wrapAsync(async(req,res)=>{
-    try{
-   let {username,email,password} = req.body;
-   const newuser = new User({email,username});
-   const registeredUser = await User.register(newuser,password);
-   console.log(registeredUser);
-   req.login(registeredUser,(err)=>{
-    if(err){
-        return next(err);
-    }
-    req.flash("success","Welcome to WanderNest!");
-    res.redirect("/listings");
-   });
-    }
-    catch(e){
-        req.flash("error",e.message);
-        res.redirect("/signUp");
-    }
-   
-}));
-router.get("/login",(req,res)=>{
-    res.render("users/login.ejs");
-});
-router.post("/login",saveRedirectUrl,passport.authenticate("local",{failureFlash:true,failureRedirect:"/login"}),async(req,res)=>{
-    req.flash("success","Welcome back!");
-    res.redirect(res.locals.redirectUrl || "/listings");
-});
-router.get("/logout",(req,res,next)=>{
-    req.logout((err)=>{
-        if(err){
-            next(err);
-        }
-        req.flash("success","You are logged out now!");
-        res.redirect("/listings");
-    });
-});
+const UserController = require('../controllers/user.js');
+
+router.get("/signUp", UserController.RenderSignUpForm);
+
+router.post("/signUp", wrapAsync(UserController.signUp));
+
+router.get("/login", UserController.RenderLoginForm);
+
+router.post("/login",saveRedirectUrl,passport.authenticate("local",{failureFlash:true,failureRedirect:"/login"}),UserController.login);
+
+router.get("/logout",UserController.logout);
+
 module.exports = router;
