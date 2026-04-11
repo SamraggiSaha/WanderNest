c1fc11948c9a13041eb3415af7455e5e446c2393
SamraggiSaha <sahasamraggi2005@gmail.com>
2026-04-12 00:50:22 +0530

    AirBnB Clone

--- routes/review.js
+++ routes/review.js
@@ -6,24 +6,12 @@ const { listingSchema, reviewSchema } = require('../schema.js');
 const Review= require('../models/review.js');
 const Listing = require('../models/listing.js');
 const {validateReview,isLoggedin,isReviewAuthor} = require('../middleware.js');
+const ReviewController = require('../controllers/review.js');
 
 //post review route
-router.post("/",isLoggedin,validateReview,wrapAsync(async(req,res)=>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    listing.reviews.push(newReview);
-    await listing.save();
-    await newReview.save();
-     req.flash("success","New Review Created Successfully");
-    console.log(newReview);
-    res.redirect(`/listings/${listing._id}`);
-}));
-router.delete("/:reviewId",isLoggedin,isReviewAuthor,wrapAsync(async(req,res)=>{
-    let {id,reviewId}= req.params;
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-     req.flash("success","Review Deleted Successfully");
-    res.redirect(`/listings/${id}`);
-}));
+router.post("/",isLoggedin,validateReview,wrapAsync(ReviewController.createReview));
+
+//delete review route 
+router.delete("/:reviewId",isLoggedin,isReviewAuthor,wrapAsync(ReviewController.deleteReview));
+
 module.exports = router;
